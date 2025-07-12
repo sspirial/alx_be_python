421085f4bdd437910c72927962b2eda46f133b3f
Munubi <sspirial@gmail.com>
2025-07-12 19:43:17 +0300

    feat: arithmetic operations in python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1, number2 = 10, 5
+
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
