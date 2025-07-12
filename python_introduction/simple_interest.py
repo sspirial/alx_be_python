665808fc52ae73a2c308ed03e4e423d57dd63b92
Munubi <sspirial@gmail.com>
2025-07-12 19:53:01 +0300

    fix: decoupling the calc from the result

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,4 +2,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-print(f"The simple interest is: {principal * rate * time}")
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
