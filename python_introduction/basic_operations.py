859b8bf552810cd89a5ee38105c77736615b2358
Munubi <sspirial@gmail.com>
2025-07-12 19:46:35 +0300

    fix: regular variable assignment

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,5 @@
-number1, number2 = 10, 5
+number1 = 10
+number2 = 5
 
 print(f"Addition of {number1} and {number2} is {number1 + number2}")
 print(f"subtraction of {number1} and {number2} is {number1 - number2}")
