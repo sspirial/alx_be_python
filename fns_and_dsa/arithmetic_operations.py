57ce0329d9e1beb78f397cf3a0dc4888b2587f77
Munubi Emmanuel <sspirial@gmail.com>
2025-07-27 18:54:45 +0300

    week 5 done

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation): #! Parameters: num1 (float), num2 (float), and operation (string). The operation parameter accepts four possible values: 'add', 'subtract', 'multiply', or 'divide'.
+    if operation == 'add':
+        #! The function should execute the arithmetic operation based on the operation parameter and the numerical values provided.
+        return num1 + num2
+    elif  operation == 'subtract':
+        return num1 - num2  
+    elif  operation == 'multiply':
+        return num1 * num2  
+    elif  operation == 'divide':
+        if num2 == 0: #! For division, include handling for division by zero, returning a specific message or value that your main.py script can recognize and display appropriately.
+            return('cannot divide by 0') 
+        else:
+            return num1 / num2   #! Return the result of the arithmetic operation.
---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,41 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+            
+        if choice == '1':
+            # Prompt for and add an item
+            item = input('Enter the item to add: ')
+            shopping_list.append(item)
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            item = input('Enter the item to remove: ')
+            if item in shopping_list:
+                shopping_list.remove(item)
+            else:
+                print('This item not in your shopping list')
+
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            for i in shopping_list:
+                print(i)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
