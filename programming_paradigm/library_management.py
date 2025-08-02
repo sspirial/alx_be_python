916b02eb7a514b97a8a674532ed1e8db99482743
Munubi Emmanuel <sspirial@gmail.com>
2025-08-02 13:09:11 +0300

    task: Implementing Basic OOP for a Library Management System

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,34 @@
+class Book:
+    def __str__(self):
+        return f"{self.title} by {self.author}" 
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+    
+
+class Library:
+    def __init__(self):
+        self._books = []
+    
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and not book._is_checked_out:
+                book._is_checked_out = True
+                return book
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and book._is_checked_out:
+                book._is_checked_out = False
+                return book
+        return None
+
+    def list_available_books(self):
+        available = [book for book in self._books if not book._is_checked_out]
+        for book in available:
+            print(book)
+        return available
