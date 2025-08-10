c411987ac93a7a53409f492c6049ee1fafd413af
Munubi Emmanuel <sspirial@gmail.com>
2025-08-11 01:07:01 +0300

    Refactor Book class string representation

--- oop/library_system.py
+++ oop/library_system.py
@@ -7,7 +7,7 @@ class Book:
         print(f"Deleting {self.title}")
 
     def __str__(self):
-        return f"{self.title} by {self.author} "
+        return f"Book: {self.title} by {self.author}"
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}')"
@@ -21,7 +21,7 @@ class EBook(Book):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
 
     def __repr__(self):
-        return f"Ebook('{self.title}', '{self.author}', {self.file_size})"
+        return f"EBook('{self.title}', '{self.author}', '{self.file_size}')"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -33,7 +33,7 @@ class PrintBook(Book):
 
     def __repr__(self):
         return f"PrintBook('{self.title}', '{self.author}', {self.page_count})"
-    
+
 class Library:
     def __init__(self):
         self.books = []
